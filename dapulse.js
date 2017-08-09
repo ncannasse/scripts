6d0951f6940aef499a2cec31e2668d45b699124f
Nicolas Cannasse <ncannasse@gmail.com>
2017-08-09 17:25:06 +0200

    fix

--- dapulse.js
+++ dapulse.js
@@ -1,11 +1,18 @@
 function setHandlers() {
   if( typeof($) == undefined ) return;
+  
+  
+  // right click to open popup menu
   var it =$('.pulse_item').not(".setup");
-  it.addClass("setup");
+  it.addClass("setup");  
   it.contextmenu(function(e) {
         e.preventDefault();
         $(this).find('.pulse_menu_btn').click();
   });
+  
+  // disable overlay gray when over right menu
+  $(".pulse-overlay").css("background","transparent");
+  
 };
 setInterval(setHandlers,500);
 
